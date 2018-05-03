8782aa20f096f09b3fbd72e03e1c5c4d7b83901f
北䑓如法 <algebraicallyClosedField@gmail.com>
2018-05-03 15:32:08 +0900

    make sure venues

---
+++ public/javascripts/script.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+  init();
+}
+
+function init() {
+  Vue.component('venue-item', {
+    props: ['item'],
+    template: `
+      <div class="col-md-8 col-lg-4 mt-4 mt-lg-0">
+        <div class="fdb-box">
+          <div class="row no-gutters align-items-center">
+            <div class="col-3">
+              <img alt="image" class="img-fluid" src="images/white.png">
+            </div>
+            <div class="col-8 ml-auto">
+              <p>
+                <strong><a v-bind:href="item.url">{{ item.title }}</a></strong>
+                <br>
+              </p>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-12">
+              <p class="text-h3" v-html="item.description">
+              </p>
+              <p v-if="item.name !== ''" class="text-h3">
+                by {{ item.name }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  })
+
+  const xhr = new XMLHttpRequest();
+  const url = 'https://script.google.com/macros/s/AKfycbwUVB2P22NgTMJmoMFTK4c66rX53UWLahm-q-uwK4v1AtX76nFx/exec';
+  xhr.open("GET", url, true);
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 304)) {
+      data = xhr.response;
+      var app = new Vue({
+        el: '#app',
+        data: {
+          venues: data
+        }
+      });
+      twttr.widgets.load();
+    }
+  }
+  xhr.send();
+}
+
